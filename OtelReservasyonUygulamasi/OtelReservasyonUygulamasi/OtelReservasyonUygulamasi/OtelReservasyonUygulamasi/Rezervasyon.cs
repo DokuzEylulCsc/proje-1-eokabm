1c8e4d6925d24aa7b17ecf4b3d9fe4d03e6d4734
bugramogol <48065838+bugramogol@users.noreply.github.com>
2019-04-05 18:07:27 +0300

    Add files via upload
    Version 0.3

---
+++ OtelReservasyonUygulamasi/OtelReservasyonUygulamasi/OtelReservasyonUygulamasi/OtelReservasyonUygulamasi/Rezervasyon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace OtelReservasyonUygulamasi
+{
+    class Rezervasyon
+    {
+        private int no;
+        private int kisi_sayisi;
+        private DateTime baslangic_tarihi;
+        private DateTime bitis_tarihi;
+        private static int rez_no = 0;
+
+        public Rezervasyon(int kisiSayisi, DateTime basTar, DateTime bitTar)
+        {
+            rez_no++;
+            this.no = rez_no;
+            this.kisi_sayisi = kisiSayisi;
+            this.baslangic_tarihi = basTar;
+            this.bitis_tarihi = bitTar;
+        }
+
+        public int No
+        {
+            get
+            {
+                return no;
+            }
+        }
+       
+        public int Kisi_sayisi
+        {
+            get
+            {
+                return kisi_sayisi;
+            }
+        }
+
+        public DateTime Baslangic_Tarihi
+        {
+            get
+            {
+                return baslangic_tarihi;
+            }
+        }
+
+        public DateTime Bitis_Tarihi
+        {
+            get
+            {
+                return bitis_tarihi;
+            }
+        }
+
+
+
+    }
+}
